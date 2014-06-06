ca858e3a895c41f26d67f4a0da409b94bf47122e
Shali Nguyen <shalinguyen@gmail.com>
2014-06-05 19:32:24 -0700

    Tip Calculator!

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Shali Nguyen on 6/3/14.
+//  Copyright (c) 2014 shali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Shali Nguyen on 6/3/14.
+//  Copyright (c) 2014 shali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
